b82d6c56849232c219fb62dd8011853fd11090c6
yugaldhiman <yugaldhiman14@gmail.com>
2025-03-25 11:39:46 +0530

    change image

--- src/Component/Hero/Hero.jsx
+++ src/Component/Hero/Hero.jsx
@@ -2,31 +2,31 @@ import React from 'react'
 import "./hero.css"
 import handIcon from "../Assest/hand_icon.png"
 import arrowIcon from "../Assest/arrow.png"
-import heroImage from "../Assest/hero_image.png"
+import heroImage from "../Assest/hero_image1.png"
 
 function Hero() {
-  return (
-    <div className='hero'>
-        <div className="hero-left">
-            <h2>NEW ARRIVALS ONLY</h2>
-            <div>
-            <div className="hero-hand-icon">
-                <p>New</p>
-                <img src={handIcon} alt="" />
+    return (
+        <div className='hero'>
+            <div className="hero-left">
+                <h2>NEW ARRIVALS ONLY</h2>
+                <div>
+                    <div className="hero-hand-icon">
+                        <p>New</p>
+                        <img src={handIcon} alt="" />
+                    </div>
+                    <p>Collections</p>
+                    <p>For Everone</p>
+                </div>
+                <div className="hero-latest-button">
+                    <div>Latest Collection</div>
+                    <img src={arrowIcon} alt="" />
+                </div>
             </div>
-            <p>Collections</p>
-            <p>For Everone</p>
+            <div className="hero-right">
+                <img src={heroImage} alt="" />
+            </div>
         </div>
-        <div className="hero-latest-button">
-            <div>Latest Collection</div>
-            <img src={arrowIcon} alt="" />
-        </div>
-        </div>
-        <div className="hero-right">
-            <img src={heroImage} alt="" />
-        </div>
-    </div>
-  )
+    )
 }
 
 export default Hero
